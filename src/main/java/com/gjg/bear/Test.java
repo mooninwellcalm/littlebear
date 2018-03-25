e381123fbd2eb70929445a9b2758f21e395b41af
mooninwellcalm <mooninwellcalm@126.com>
2018-03-25 22:22:10 +0800

    test

--- src/main/java/com/gjg/bear/Test.java
+++ src/main/java/com/gjg/bear/Test.java
@@ -2,6 +2,14 @@ package com.gjg.bear;
 
 public class Test {
 
+    private int mmm;
+
+    public Test(){
+        int i;
+        int j;
+        int k;
+    }
+
     public static void main(String[] args) {
         System.out.println("it is ok");
     }
